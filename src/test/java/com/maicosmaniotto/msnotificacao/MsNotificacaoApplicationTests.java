a99378d4b718abcdfe4eac12af05c0d61fed5298
maico-smaniotto <maicosmaniotto@gmail.com>
2025-05-08 23:08:08 -0300

    Inicializado o projeto

---
+++ src/test/java/com/maicosmaniotto/msnotificacao/MsNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maicosmaniotto.msnotificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
